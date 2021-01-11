be35534d12bed7c11a701901661d6192f5dd9cf9
Adam Tak <adam.tak2112@gmail.com>
2021-01-10 18:53:22 -0700

    6:00 mark of 18

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -27,9 +27,11 @@ const jHeight = 1.76;
 
 const mBMI = mMass / mHeight ** 2;
 
+//added a function to calculate BMI
 function calculateBMI(mass, height){
     return  mass / height ** 2;
 }
+//taking an OOP approach to the challenge: could easily implement into a class
 function markHigherBMI(){
     if (calculateBMI(mMass, mHeight) > calculateBMI(jMass, jHeight)){
         return true;
@@ -38,7 +40,7 @@ function markHigherBMI(){
 }
 
 console.log(markHigherBMI());
-*/
+
 
 const firstName = 'Jonas';
 const job = 'Teacher';
@@ -51,3 +53,15 @@ console.log(jonas);
 
 const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
 console.log(jonasNew);
+// you can use ``s in place of all ""s and ''s
+console.log(`Just a regular string...`);
+*/
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough){
+    console.log('Sarah can start driving lessons');
+} else{
+    console.log(`Sarah cannot drive yet!`);
+}
