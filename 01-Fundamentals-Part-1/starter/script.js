b8327954da4e060b9363681fa18e28b2c5486f8a
Adam Tak <adam.tak2112@gmail.com>
2021-01-10 18:38:32 -0700

    coding challenge 1
    added some functions, for cleaner code

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -7,5 +7,34 @@ let language;
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
-console.log(typeof language);*/
+console.log(typeof language);
 
+//let if you need to change variables
+let age = 30;
+age = 31;
+//const if the variable does not change in program
+const birthYear = 1991;
+const job;
+
+//DO NOT USE VAR
+var job = 'programmer';
+job = 'teacher';*/
+
+const mMass = 78;
+const mHeight = 1.69;
+const jMass = 92;
+const jHeight = 1.95;
+
+const mBMI = mMass / mHeight ** 2;
+
+function calculateBMI(mass, height){
+    return  mass / height ** 2;
+}
+function markHigherBMI(){
+    if (calculateBMI(mMass, mHeight) > calculateBMI(jMass, jHeight)){
+        return true;
+    }
+    return false;
+}
+
+console.log(markHigherBMI());
