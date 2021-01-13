cd6e5543fabd7ad985fc6719d7148831acc4fa6a
Adam Tak <adam.tak2112@gmail.com>
2021-01-13 15:45:43 -0700

    through logical operator lecture

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -177,7 +177,7 @@ console.log('123' < 57);
 //prediction: 1143
 console.log(5 + 6 + '4' + 9 - 4 - 2);
 //all predictions correct!!
-*/
+
 
 const age = 18;
 //=== strict equals, only returns true if type and value are the same
@@ -199,5 +199,16 @@ if (favorite === 23) {
 if (favorite !== 23){
     console.log('why not 23?');
 }
+*/
 
+const numNeighbors = Number(prompt('How many neighbor countries does your country have?'));
 
+if (numNeighbors === 1){
+    console.log("Only 1 border!");
+} else if (numNeighbors > 1){
+    console.log("More than 1 border");
+} else{
+    console.log("no neighbors");
+}
+
+//skipped boolean variable lecture
