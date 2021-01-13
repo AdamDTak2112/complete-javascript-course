9435fa73880a78647dec5f7be07d1f1a02fc0316
Adam Tak <adam.tak2112@gmail.com>
2021-01-13 16:21:21 -0700

    exercise on switch statement logic

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -211,7 +211,7 @@ if (numNeighbors === 1){
 }
 
 //skipped boolean variable lecture
-*/
+
 
 
 
@@ -230,4 +230,47 @@ if (scoreDolphins >= 100 || scoreKoalas >= 100) {
     }
 } else {
     console.log("Sorry, no winners today!");
+}
+
+
+const day = prompt("what day?");
+//switch statement lecture
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+*/
+const day = prompt("what day?");
+//converted into if/else statements
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+} else {
+    console.log('Not a valid day!');
 }
