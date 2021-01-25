b617ff1816a704b9331b55ea3aa07effdc8dd26f
Adam Tak <adam.tak2112@gmail.com>
2021-01-25 12:05:42 -0700

    rewrote problem to account for type difference of sensor errors

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -51,11 +51,22 @@ const tempteratures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - Find difference of max and min
 
 const calcTempAmp = function (temps) {
-    const maxTemp = Math.max(...temps);
+    /*const maxTemp = Math.max(...temps);
     console.log(temps);
     const minTemp = Math.min(...temps);
     console.log(minTemp);
     const amplitude = maxTemp - minTemp;
-    return amplitude;
+    return amplitude;*/
+    let max = temps[0];
+    let min = temps[0];
+    for (let i = 0; i < temps.length; i++) {
+        const currentTemp = temps[i];
+
+        if (typeof currentTemp !== "number") continue;
+        if (currentTemp > max) max = currentTemp;
+        if (currentTemp < min) min = currentTemp;
+
+    }
+    return max - min;
 }
-console.log(calcTempAmp([3, 7, 4]));
+console.log(calcTempAmp(tempteratures));
