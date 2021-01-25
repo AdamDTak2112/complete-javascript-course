8825af77d87ab98e5f8c21c50b52990f390cae5b
Adam Tak <adam.tak2112@gmail.com>
2021-01-25 12:00:47 -0700

    simple problem solving exercise

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -33,3 +33,29 @@ console.log(calcAge(1991));
 //  Research is a vital part of development
 
 //4. Write pseudo-code before writing actual code, for bigger problems
+
+// PROBLEM:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be sensor error".
+
+const tempteratures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understand the problem
+// - What is temperature amplitude? Difference between lowest and highest numbers in array
+// - How to calculate max and min temperatures?
+// - What is sensor error? What to do when it occurs?
+
+// 2) Breaking up into sub-problems
+// - Ignore sensor errors
+// - Find max value in temp array
+// - Find min value in temp array
+// - Find difference of max and min
+
+const calcTempAmp = function (temps) {
+    const maxTemp = Math.max(...temps);
+    console.log(temps);
+    const minTemp = Math.min(...temps);
+    console.log(minTemp);
+    const amplitude = maxTemp - minTemp;
+    return amplitude;
+}
+console.log(calcTempAmp([3, 7, 4]));
