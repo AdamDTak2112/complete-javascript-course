42ce97ecbde8615fc4e3afab830a37c3fcf31b00
Adam Tak <adam.tak2112@gmail.com>
2021-01-25 11:45:36 -0700

    problem solving notes

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -2,10 +2,34 @@
 'use strict';
 
 
-
+/*
 const x = 23;
 if (x === 23) console.log(23);
 
 const calcAge = birthYear => 2037 - birthYear;
 
 console.log(calcAge(1991));
+*/
+
+//4 steps to solve any problem
+//1. Make sure you understand the problem 100%. Ask the right questions to get a clear picture of the problem.
+//  ex. Project Manager: "We need a function that reverses whatever we pass into it"
+//  1. what does whatever mean? what is being passed in?
+//  2. What do we do if something unexpected passes in?
+//  3. What should be returned?
+//  4. What does "reversed" mean in this context?
+//  5. How to recognize what type the argument is
+//  6. How to reverse something of any type
+
+//2. Divide and conquer: Break a big problem into smaller sub-problems
+//  sub problems:
+//  1. check if argument is a number, a string, or an array
+//  2. Implement reversing a number
+//  3. Implement reversing a string
+//  4. Implement reversing an array
+//  5. Return reversed value
+
+//3. Research as much as you need to
+//  Research is a vital part of development
+
+//4. Write pseudo-code before writing actual code, for bigger problems
